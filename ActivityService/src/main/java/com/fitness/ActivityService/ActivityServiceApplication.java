60cd4591231bfe700a6cad40a944c982c170e998
Amit Kumar <kumaramit1996.1@gmail.com>
2025-09-29 05:37:56 +0530

    ActivityServiceApplication

---
+++ ActivityService/src/main/java/com/fitness/ActivityService/ActivityServiceApplication.java
@@ -0,0 +1,18 @@
+package com.fitness.ActivityService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityServiceApplication {
+
+
+
+	public static void main(String[] args) {
+
+		System.out.println("Jay Shri Ram! ActivityServiceApplication Start ");
+		SpringApplication.run(ActivityServiceApplication.class, args);
+		System.out.println("Jay Shri Ram! ActivityServiceApplication End ");
+	}
+
+}
