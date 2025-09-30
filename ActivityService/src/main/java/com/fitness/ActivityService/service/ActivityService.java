2f68e9390a6c479a8ad76f1fd52e1e20d72989de
Amit Kumar <kumaramit1996.1@gmail.com>
2025-09-30 22:14:59 +0530

    ActivityServiceApplication

--- ActivityService/src/main/java/com/fitness/ActivityService/service/ActivityService.java
+++ ActivityService/src/main/java/com/fitness/ActivityService/service/ActivityService.java
@@ -3,19 +3,13 @@ package com.fitness.ActivityService.service;
 
 import com.fitness.ActivityService.dto.ActivityRequest;
 import com.fitness.ActivityService.dto.ActivityResponce;
-import com.fitness.ActivityService.model.ActivityType;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.Map;
-import java.util.Objects;
 
 @Service
 public class ActivityService {
 
     public ActivityResponce trackActivity(ActivityRequest request) {
+
+        return null;
     }
 }
