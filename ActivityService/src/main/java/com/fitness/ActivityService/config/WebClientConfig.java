71084d77f049c7b0c33dc8a6c9f9722a03345049
Amit Kumar <kumaramit1996.1@gmail.com>
2025-10-02 03:19:07 +0530

    WebClientConfig

--- ActivityService/src/main/java/com/fitness/ActivityService/config/WebClientConfig.java
+++ ActivityService/src/main/java/com/fitness/ActivityService/config/WebClientConfig.java
@@ -5,21 +5,40 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+/**
+ * WebClientConfig is a configuration class for defining WebClient beans.
+ * WebClient is a reactive, non-blocking HTTP client in Spring WebFlux.
+ */
 @Configuration
 public class WebClientConfig {
 
-    // Define a LoadBalanced WebClient.Builder bean
+    /**
+     * Creates a WebClient.Builder bean that is load balanced.
+     *
+     * @LoadBalanced tells Spring Cloud to use Ribbon (or Spring Cloud LoadBalancer)
+     *               to resolve service names like "USERSERVICE" to actual instances
+     *               registered in Eureka.
+     *
+     * This bean can be injected wherever you need a WebClient.Builder.
+     */
     @Bean
     @LoadBalanced
     public WebClient.Builder webClientBuilder() {
+        // Returns a builder to create WebClient instances
         return WebClient.builder();
     }
 
-    // Example: WebClient for UserService
+    /**
+     * Creates a WebClient specifically configured to call the UserService.
+     *
+     * @param webClientBuilder the LoadBalanced WebClient.Builder injected by Spring
+     * @return a WebClient instance ready to call UserService endpoints
+     */
     @Bean
     public WebClient userServiceWebClient(WebClient.Builder webClientBuilder) {
         return webClientBuilder
-                .baseUrl("http://USERSERVICE") // service id from Eureka or service discovery
-                .build();
+                .baseUrl("http://USERSERVICE") // USERSERVICE is the application name registered in Eureka
+                .build();                       // Build the WebClient instance
     }
+
 }
