8499f99a000887a48e1663f88b2b1ca80c53ccac
Amit Kumar <kumaramit1996.1@gmail.com>
2025-10-03 05:46:25 +0530

    controller

---
+++ aiService/src/main/java/com/fitness/aiService/controller/RecommendationController.java
@@ -0,0 +1,36 @@
+package com.fitness.aiService.controller;
+
+import lombok.RequiredArgsConstructor;
+import model.Recommendation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/recommendations")
+public class RecommendationController {
+
+    private final RecommendationService recommendationService;
+
+    @GetMapping("/user/{UserId}")
+    public ResponseEntity<List<Recommendation>> getUserRecommendation(@PathVariable String UserId) {
+        return ResponseEntity.ok(recommendationService.getUserRecommendation(UserId));
+    }
+
+
+    @GetMapping("/activity/{activityId}")
+    public ResponseEntity<Recommendation>
+    getActivityRecommendation(@PathVariable String activityId) {
+
+        return ResponseEntity.ok(recommendationService.getActivityRecommendation(activityId));
+
+
+    }
+
+
+}
