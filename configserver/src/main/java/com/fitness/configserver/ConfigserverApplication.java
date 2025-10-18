7aebdd584b29d7bad2f222bca08d9537ead715da
Amit Kumar <kumaramit1996.1@gmail.com>
2025-10-18 21:01:11 +0530

    configuration service

--- configserver/src/main/java/com/fitness/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/fitness/configserver/ConfigserverApplication.java
@@ -2,12 +2,18 @@ package com.fitness.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+
+@EnableConfigServer
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ConfigserverApplication.class, args);
+		System.out.println("Jay shri Ram!");
 	}
 
 }
