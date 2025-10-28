f5f5e6efff5c73267b5f9171da7a2ab822b56413
Amit Kumar <kumaramit1996.1@gmail.com>
2025-10-28 23:21:09 +0530

    Spring Security

--- gateway/src/main/java/com/fitness/gateway/keycloakUserSyncFilter.java
+++ gateway/src/main/java/com/fitness/gateway/keycloakUserSyncFilter.java
@@ -56,6 +56,8 @@ public class keycloakUserSyncFilter implements WebFilter {
                         }
                     })
                     .then(Mono.defer(()->{
+
+
                         ServerHttpRequest mutatedRequset = (ServerHttpRequest) exchange.getRequest().mutate()
                                 .header("X-User-ID", finalUserId)
                                 .build();
