4b321d3bf82f2f66f2adbddc2a53f13995609415
Amit Kumar <kumaramit1996.1@gmail.com>
2025-11-05 19:56:22 +0530

    Spring Security

--- configserver/src/main/java/com/fitness/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/fitness/configserver/ConfigserverApplication.java
@@ -15,6 +15,8 @@ public class ConfigserverApplication {
 	public static final String RESET = "\u001B[0m";
 
 
+
+
 	public static final String GREEN = "\u001B[32m";
 
 	public static final String YELLOW = "\u001B[33m";
