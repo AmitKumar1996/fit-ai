fc9b9626606377a4873292b3105b2260c89f6dfb
Amit Kumar <kumaramit1996.1@gmail.com>
2025-11-10 23:36:52 +0530

    Spring Security

--- configserver/src/main/java/com/fitness/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/fitness/configserver/ConfigserverApplication.java
@@ -16,10 +16,6 @@ public class ConfigserverApplication {
 	// ANSI color codes
 
 	public static final String RESET = "\u001B[0m";
-
-
-
-
 	public static final String GREEN = "\u001B[32m";
 
 	public static final String YELLOW = "\u001B[33m";
